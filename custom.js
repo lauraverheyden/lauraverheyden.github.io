87adb2799235d9554c1e4dcdc40bbb058c294bea
Laura Verheyden <verheydenlaura.design@gmail.com>
2019-07-03 21:23:29 +0200

    ok

---
+++ custom.js
@@ -0,0 +1,9 @@
+$('li.dropdown').on('click', function() {
+    var $el = $(this);
+    if ($el.hasClass('open')) {
+        var $a = $el.children('a.dropdown-toggle');
+        if ($a.length && $a.attr('href')) {
+            location.href = $a.attr('href');
+        }
+    }
+});
